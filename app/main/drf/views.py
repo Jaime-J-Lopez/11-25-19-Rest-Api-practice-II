52c0ba7dd5a70184031ded6fe75162fb1e083d97
Jaime Lopez <geekwise.jaime.lopez@gmail.com>
2019-11-25 21:19:06 +0000

    fully functional

---
+++ app/main/drf/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import User_Serializer, Group_Serializers
+
+
+class User_Viewset( viewsets.ModelViewSet ):
+    '''
+    API endpoint that allows users to be viewed or edited
+    '''
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = User_Serializer
+
+class Group_Viewset( viewsets.ModelViewSet ):
+    '''
+    API endpoint that allows groups to be viewed or edited
+    '''
+    queryset = Group.objects.all()
+    serializer_class = Group_Serializers
